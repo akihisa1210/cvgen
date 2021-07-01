229d9cdc4f11546c49bd7f11419f4c8549f04453
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2021-07-01 22:35:21 +0900

    refactor: simplify if statement

--- cmd/cvgen/main.go
+++ cmd/cvgen/main.go
@@ -10,11 +10,6 @@ import (
 )
 
 func main() {
-	var (
-		input  string
-		output string
-	)
-
 	cli.VersionFlag = &cli.BoolFlag{
 		Name:    "version",
 		Aliases: []string{"v"},
@@ -27,17 +22,15 @@ func main() {
 		Usage:   "generate curriculum vitae",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
-				Name:        "input",
-				Aliases:     []string{"i"},
-				Usage:       "input from `FILE`",
-				Destination: &input,
-				Required:    true,
+				Name:     "input",
+				Aliases:  []string{"i"},
+				Usage:    "input from `FILE`",
+				Required: true,
 			},
 			&cli.StringFlag{
-				Name:        "output",
-				Aliases:     []string{"o"},
-				Usage:       "output to `FILE`",
-				Destination: &output,
+				Name:    "output",
+				Aliases: []string{"o"},
+				Usage:   "output to `FILE`",
 			},
 			&cli.BoolFlag{
 				Name:    "markdown",
@@ -46,7 +39,7 @@ func main() {
 			},
 		},
 		Action: func(c *cli.Context) error {
-			f, err := os.Open(input)
+			f, err := os.Open(c.String("input"))
 			if err != nil {
 				return cli.Exit(err, 1)
 			}
@@ -57,43 +50,28 @@ func main() {
 				return cli.Exit(err, 1)
 			}
 
+			var op string
+
 			if c.Bool("markdown") {
-				md, err := career.MarkDownGenerate(cr)
-				if err != nil {
-					return cli.Exit(err, 1)
-				}
-
-				if output == "" {
-					fmt.Println(md)
-					return nil
-				}
-
-				out, err := os.Create(output)
-				if err != nil {
-					return cli.Exit(err, 1)
-				}
-				defer out.Close()
-				out.Write([]byte(md))
-
-				return nil
+				op, err = career.MarkDownGenerate(cr)
+			} else {
+				op, err = career.HTMLGenerate(cr)
 			}
-
-			html, err := career.HTMLGenerate(cr)
 			if err != nil {
 				return cli.Exit(err, 1)
 			}
 
-			if output == "" {
-				fmt.Println(html)
+			if c.String("output") == "" {
+				fmt.Println(op)
 				return nil
 			}
 
-			out, err := os.Create(output)
+			out, err := os.Create(c.String("output"))
 			if err != nil {
 				return cli.Exit(err, 1)
 			}
 			defer out.Close()
-			out.Write([]byte(html))
+			out.Write([]byte(op))
 
 			return nil
 		},
